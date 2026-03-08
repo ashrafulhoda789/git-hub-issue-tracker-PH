58617dbe744497e412eb19e5a419a6264b4bf366
ashraful-hoda <asrafulhoda789@gmail.com>
2026-03-09 01:58:49 +0600

    add modal and make it functional

--- script/home.js
+++ script/home.js
@@ -17,6 +17,8 @@ const loadSpinner = (status) =>{
 }
 
 
+
+
 const loadAllIssues = async() =>{
     loadSpinner(true);
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
@@ -65,6 +67,48 @@ closedBtn.addEventListener('click',
     }
 );
 
+const loadIssuesDetails = async(id) =>{
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+
+    const data = await res.json();
+    displayModal(data.data);
+}
+
+const displayModal = (issue) =>{
+    console.log(issue);
+    const detailsBox = document.getElementById("details-container");
+    detailsBox.innerHTML = `
+        <div class="space-y-5">
+            <div>
+                <h1 class="text-2xl dark-blue font-bold">${issue.title}</h1>
+            </div>
+            <div class="flex items-center gap-10">
+                <p class=" rounded-2xl p-2 ${issue.status.toUpperCase() === 'CLOSED' ? "text-[#A855F7] bg-purple-100" : "text-[#00A96E] bg-green-100"}">${issue.status}</p>
+                <li>Open By: ${issue.author}</li>
+                <li>${issue.createdAt}</li>
+            </div>
+            <div>
+                ${createElement(issue.labels)}
+            </div>
+
+            <div>
+                <p class="text-[#64748B]">${issue.description}</p>
+            </div>
+            <div class="flex justify-between items-center px-5 py-2 bg-gray-100 rounded-lg">
+                <div>
+                    <h2 class="text-[#64748B]">Assignee:</h2>
+                    <h2 class="dark-blue font-semibold">${issue.assignee}</h2>
+                </div>
+                <div class="flex flex-col justify-center">
+                    <h2 class="text-[#64748B]">Priority:</h2>
+                    <p class="px-3 py-2 rounded-xl font-medium ${issue.priority.toUpperCase() === 'HIGH' ? "text-red-400 bg-red-100" : issue.priority.toUpperCase() === 'LOW' ? "text-gray-600 bg-gray-200":" text-yellow-600 bg-yellow-100"}">${issue.priority}</p>
+                </div>
+            </div>
+        </div>
+    `;
+    document.getElementById("issue_modal").showModal();
+}
+
 const displayIssues = (issues) =>{
     // console.log(issues);
     const cardContainer = document.getElementById("cardContainer");
@@ -82,7 +126,7 @@ const displayIssues = (issues) =>{
         // console.log(issue);
         const card = document.createElement("div");
         card.innerHTML = `
-            <div class="bg-white space-y-4 rounded-2xl p-5 border-t-4 ${issue.status.toUpperCase() === 'CLOSED' ? "border-[#A855F7]" : "border-[#00A96E]"}">
+            <div onclick="loadIssuesDetails(${issue.id})" class="bg-white space-y-4 rounded-2xl p-5 border-t-4 ${issue.status.toUpperCase() === 'CLOSED' ? "border-[#A855F7]" : "border-[#00A96E]"}">
                 <div class="flex justify-between items-center">
                     <img src="${issue.status.toUpperCase() === 'CLOSED' ? "assets/Closed- Status .png" : 'assets/Open-Status.png'}" alt="">
                     <p class="px-3 rounded-xl ${issue.priority.toUpperCase() === 'HIGH' ? "text-red-400 bg-red-100" : issue.priority.toUpperCase() === 'LOW' ? "text-gray-500 bg-gray-200":" text-yellow-500 bg-yellow-100"}  ">${issue.priority.toUpperCase()}</p>
@@ -114,14 +158,12 @@ document.getElementById("searchBtn").addEventListener('click',
         const inputValue = input.value.trim().toLowerCase();
         // console.log(inputValue);
 
-        fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${inputValue}`)
         .then((res) => res.json())
         .then((data) => {
             const allIssues = data.data;
-            const filterIssues = allIssues.filter(issue => issue.title.toLowerCase().includes(inputValue));
-
-            displayIssues(filterIssues);
+            
+            displayIssues(allIssues);
         })
-        
     }
 );
